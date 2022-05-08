a05332cd14f00a8e5bea532ca099d5f4cbeaea46
suriyaaap13 <suriyaaap13@gmail.com>
2022-05-09 01:54:13 +0530

    Remove ajax from the file

--- controller/home_controller.js
+++ controller/home_controller.js
@@ -31,28 +31,24 @@ module.exports.createData = async (req, res)=>{
         // To set two dates to two variables
         const date1 = new Date(sd);
         const date2 = new Date(ed);
-        
         // To calculate the time difference of two dates
         const Difference_In_Time = date2.getTime() - date1.getTime();
-        
+        if(Difference_In_Time<0){
+            return res.redirect('back');
+        }
         // To calculate the no. of days between two dates
         const Difference_In_Days = Difference_In_Time / (1000 * 3600 * 24)+1;
-        console.log(Difference_In_Days);
         // per day consumption
-        console.log('This is power '+req.body.power);
         const perDay = (req.body.power/Difference_In_Days).toFixed(2);
         let d = date1;
         while(d<=date2){
-            console.log("I am inside for loop");
             let endOfMonth = new Date(d.getFullYear(), d.getMonth()+1, 0);
             // if date2 is less than the end of the month.
             if(date2<endOfMonth){
                 endOfMonth = date2;
             }
-            console.log("endofMonth "+endOfMonth);
             // Find the number of days that is in the range of start of month and end of month 
             const days_diff = Math.ceil((endOfMonth.getTime() - d.getTime())/(1000 * 3600 * 24))+1;
-            console.log(endOfMonth.getTime()+" "+d.getTime()+" "+days_diff);
             const filter = {
                 year: endOfMonth.getFullYear(),
                 month: endOfMonth.getMonth()
@@ -74,7 +70,6 @@ module.exports.createData = async (req, res)=>{
             let tempDate = new Date(endOfMonth);
             d.setDate(tempDate.getDate()+1);
         }
-        console.log('this is perday '+perDay);
         return res.redirect('back');
     }catch(err){
         console.log(err);
